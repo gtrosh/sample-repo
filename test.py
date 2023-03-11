404ab659ebd518e42c23536d3e9e7317cd79324f
Galina Troshkina <maystar2009@gmail.com>
2023-03-12 01:37:42 +0300

    removed some code

--- test.py
+++ test.py
@@ -1,19 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
 
 
-print(sys.version)
-# print(sys.executable)
-def greet(who_to_greet):
-    greeting = f"Hello, {who_to_greet}"
-    return greeting
-
-
-# print(greet("World"))
-# print(greet("Lina"))
-# print(greet("Lesley"))
 r = requests.get("https://www.google.com")
 print(r.status_code)
+print(r.ok)
